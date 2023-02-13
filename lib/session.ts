2473b09ad9ccf8e16eae7d5bd1cfdde6a13a6165
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-02-13 12:02:31 +0100

    Fix getSession with authOptions

--- lib/session.ts
+++ lib/session.ts
@@ -3,11 +3,10 @@ import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/lib/auth"
 
 export async function getSession() {
-  return await getServerSession()
+  return await getServerSession(authOptions)
 }
 
 export async function getCurrentUser() {
   const session = await getSession()
-
   return session?.user
 }
