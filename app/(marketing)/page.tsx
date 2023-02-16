23fe67849f6fd34808c6a2a7c2bbac885f8c3b01
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-02-16 01:49:50 +0100

    Fix Get Starter link

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -48,7 +48,7 @@ export default async function IndexPage() {
         </div>
         <div className="flex gap-4">
           <Link
-            href="/login"
+            href="/signup"
             className="relative inline-flex h-11 items-center rounded-md border border-transparent bg-brand-500 px-8 py-2 font-medium text-white hover:bg-brand-400 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
           >
             Get Started
