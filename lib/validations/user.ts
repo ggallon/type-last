14123372e62e61898ed861cfbe6f790da1e9a7df
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-02-20 11:18:40 +0100

    feat: add api for update username

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -3,3 +3,7 @@ import * as z from "zod"
 export const userNameSchema = z.object({
   name: z.string().min(3).max(32),
 })
+
+export const userUserNameSchema = z.object({
+  username: z.string().min(3).max(48),
+})
