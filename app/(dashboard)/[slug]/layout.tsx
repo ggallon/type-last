c6714c40fb4be5c896d7b655add53b87beaed45e
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-02-20 10:21:45 +0100

    feat: add User or Team Dashboard

---
+++ app/(dashboard)/[slug]/layout.tsx
@@ -0,0 +1,21 @@
+import { DashboardNav } from "@/components/dashboard/nav"
+import { dashboardConfig } from "@/config/dashboard"
+
+interface UserOrTeamDashboardLayoutProps {
+  children?: React.ReactNode
+}
+
+export default async function UserOrTeamDashboardLayout({
+  children,
+}: UserOrTeamDashboardLayoutProps) {
+  return (
+    <>
+      <aside className="hidden w-[200px] flex-col md:flex">
+        <DashboardNav items={dashboardConfig.sidebarNav} />
+      </aside>
+      <main className="flex w-full flex-1 flex-col overflow-hidden pt-4">
+        {children}
+      </main>
+    </>
+  )
+}
---
+++ app/(dashboard)/[slug]/page.tsx
@@ -0,0 +1,86 @@
+import { cache } from "react"
+import { redirect } from "next/navigation"
+import { notFound } from "next/navigation"
+import { EmptyPlaceholder } from "@/components/dashboard/empty-placeholder"
+import { DashboardHeader } from "@/components/dashboard/header"
+import { DashboardShell } from "@/components/dashboard/shell"
+import { authOptions } from "@/lib/auth"
+import prisma, { type User } from "@/lib/db"
+import { getCurrentUser } from "@/lib/session"
+
+const getUserOrTeams = async (userId: User["id"], slug: string) => {
+  return await prisma.user.findFirst({
+    where: {
+      AND: [
+        {
+          id: userId,
+        },
+      ],
+      OR: [
+        {
+          username: slug,
+        },
+        {
+          teams: {
+            some: {
+              team: {
+                is: {
+                  slug: slug,
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
+    select: {
+      id: true,
+      name: true,
+      username: true,
+      teams: {
+        select: {
+          team: {
+            select: {
+              id: true,
+              name: true,
+              slug: true,
+            },
+          },
+        },
+      },
+    },
+  })
+}
+
+export default async function UserOrTeamDashboardPage({ params }) {
+  const user = await getCurrentUser()
+  const userOrTeam = await getUserOrTeams(user.id, params.slug)
+
+  if (!userOrTeam) {
+    return notFound()
+  }
+
+  const nameOfUserOrTeam =
+    userOrTeam.username === params.slug
+      ? userOrTeam.name
+      : userOrTeam?.teams[0].team.slug === params.slug
+      ? `${userOrTeam.teams[0].team.name} team`
+      : "unkown :("
+
+  return (
+    <DashboardShell>
+      <DashboardHeader
+        heading="Dashboard"
+        text={`Yes it's the ${nameOfUserOrTeam}!`}
+      />
+      <EmptyPlaceholder>
+        <EmptyPlaceholder.Icon name="post" />
+        <EmptyPlaceholder.Title>No project</EmptyPlaceholder.Title>
+        <EmptyPlaceholder.Description>
+          You don&apos;t have any project for the {nameOfUserOrTeam}. Start
+          creating project.
+        </EmptyPlaceholder.Description>
+      </EmptyPlaceholder>
+    </DashboardShell>
+  )
+}
