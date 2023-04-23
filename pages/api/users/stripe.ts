7acc9789ec7c77c6e81333bf2e882e41fa67a182
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-04-24 00:57:17 +0200

    refactor: stripe users api

--- pages/api/users/stripe.ts
+++ pages/api/users/stripe.ts
@@ -1,9 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import { getServerSession } from "next-auth/next"
 import { proPlan } from "@/config/subscriptions"
 import { withAuthentication } from "@/lib/api-middlewares/with-authentication"
 import { withMethods } from "@/lib/api-middlewares/with-methods"
-import { authOptions } from "@/lib/auth"
 import { stripe } from "@/lib/stripe"
 import { getUserSubscriptionPlan } from "@/lib/subscription"
 import { absoluteUrl } from "@/lib/utils"
@@ -11,57 +9,54 @@ import { absoluteUrl } from "@/lib/utils"
 const billingUrl = absoluteUrl("/account/billing")
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    try {
-      const session = await getServerSession(req, res, authOptions)
-      const user = session?.user
+  try {
+    const user = req.session?.user
 
-      if (!user || !user.email) {
-        throw new Error("User not found.")
-      }
+    if (!user || !user.email) {
+      throw new Error("User not found.")
+    }
 
-      const subscriptionPlan = await getUserSubscriptionPlan(user.id)
+    const subscriptionPlan = await getUserSubscriptionPlan(user.id)
 
-      // The user is on the pro plan.
-      // Create a portal session to manage subscription.
-      if (subscriptionPlan.isPro && subscriptionPlan.stripeCustomerId) {
-        const stripeSession = await stripe.billingPortal.sessions.create({
-          customer: subscriptionPlan.stripeCustomerId,
-          return_url: billingUrl,
-        })
-
-        return res.json({ url: stripeSession.url })
-      }
-
-      // The user is on the free plan.
-      // Create a checkout session to upgrade.
-      const stripeSession = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: proPlan.stripePriceId,
-            quantity: 1,
-          },
-        ],
-        mode: "subscription",
-        success_url: billingUrl,
-        cancel_url: billingUrl,
-        customer_email: user.email,
-        metadata: {
-          userId: user.id,
-        },
-        allow_promotion_codes: true,
-        automatic_tax: {
-          enabled: true,
-        },
-        billing_address_collection: "auto",
-        payment_method_types: ["card"],
+    // The user is on the pro plan.
+    // Create a portal session to manage subscription.
+    if (subscriptionPlan.isPro && subscriptionPlan.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: subscriptionPlan.stripeCustomerId,
+        return_url: billingUrl,
       })
 
       return res.json({ url: stripeSession.url })
-    } catch (error: any) {
-      console.error("error", error.message)
-      return res.status(500).end()
     }
+
+    // The user is on the free plan.
+    // Create a checkout session to upgrade.
+    const stripeSession = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: proPlan.stripePriceId,
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      customer_email: user.email,
+      metadata: {
+        userId: user.id,
+      },
+      allow_promotion_codes: true,
+      automatic_tax: {
+        enabled: true,
+      },
+      billing_address_collection: "auto",
+      payment_method_types: ["card"],
+    })
+
+    return res.json({ url: stripeSession.url })
+  } catch (error: any) {
+    console.error("[API]/users/stripe error: ", error.message)
+    return res.status(500).end()
   }
 }
 
